a4ecf5b7702793175290caa672e8c770f280f32a
angeliquebesi <besineaud85@gmail.com>
2021-10-07 22:35:41 +0200

    modal en mobil changement de nom

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const modal = null
+let modal = false
  function pop() {
-   if(modal === null) {
+   if(modal === false) {
      document.getElementById("overlay").style.display = "block";
      modal = true
    } else {
      document.getElementById("overlay").style.display = "none";
-     modal = null
+     modal = false
+   }
+ }
+
+ let modal1 = false
+ function pop1() {
+   if(modal === false) {
+     document.getElementById("overlay1").style.display = "block";
+     modal = true
+   } else {
+     document.getElementById("overlay1").style.display = "none";
+     modal = false
    }
  }
