42f11b5472a832e7bafcb30b573edfd8818d1d3f
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-10-07 21:08:38 +0200

    burger button

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let icon = document.querySelector('.icons')
+let logo = document.querySelector('.logo')
+let nav = document.querySelector('.navigation')
 
+let click = 0
+icon.addEventListener("click", function(event) {
+    click++
+    if (click % 2 == 0){
+        logo.style.display ="flex"
+        nav.style.display ="none"
+    }
+    if (click % 2 == 1 ){
+        logo.style.display ="none"
+        nav.style.display ="flex"
+    }
+
+
+})
+
+
+// $(window).scroll(function (event) {
+//     // A chaque fois que l'utilisateur va scroller (descendre la page)
+//     var y = $(this).scrollTop(); // On récupérer la valeur du scroll vertical
+ 
+//     //si cette valeur > à 200 on ajouter la class
+//     if (y >= 200) {
+//       $('.nav').addClass('fixed');
+//     } else {
+//       // sinon, on l'enlève
+//       $('.nav').removeClass('fixed');
+//     }
+//   });
