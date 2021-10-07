b2c004c85130e96f8b38720343be865a12270338
angeliquebesi <besineaud85@gmail.com>
2021-10-07 15:05:31 +0200

    création modal sur barre de navigation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const btnPopup = document.getElementById('btnPopup');
+const overlay = document.getElementById('overlay');
+btnPopup.addEventListener('click',openMoadl);
 
+function openMoadl() {
+    overlay.style.display='block';
+}
+
+function closeModal() {
+    overlay.style.display ='none';
+}
