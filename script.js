95ece9eaf769f597357023b14982144cabedd592
Angélique Bésineaud <74253764+angeliquebesi@users.noreply.github.com>
2021-10-08 11:00:08 +0200

    nettoyage du fichier script

--- script.js
+++ script.js
@@ -15,16 +15,6 @@ icon.addEventListener("click", function(event) {
     }
 
 })
-const modal = null
- function pop() {
-   if(modal === null) {
-     document.getElementById("overlay").style.display = "block";
-     modal = true
-   } else {
-     document.getElementById("overlay").style.display = "none";
-     modal = null
-   }
- }
 
  let modal = false
  function pop() {
@@ -47,17 +37,3 @@ const modal = null
      modal = false
    }
  }
-
-
-// $(window).scroll(function (event) {
-//     // A chaque fois que l'utilisateur va scroller (descendre la page)
-//     var y = $(this).scrollTop(); // On récupérer la valeur du scroll vertical
- 
-//     //si cette valeur > à 200 on ajouter la class
-//     if (y >= 200) {
-//       $('.nav').addClass('fixed');
-//     } else {
-//       // sinon, on l'enlève
-//       $('.nav').removeClass('fixed');
-//     }
-//   });
